4e005ca55e1f50c28dff8b8f2e4075cf841abc55
cossie101 <wcossie9@gmail.com>
2022-04-07 01:15:44 +0300

    first commit

---
+++ lib/addresses.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js"
+
+// This is your shop address
+export const shopAddress = new PublicKey('BX6Z1fasE8FaVRERWY6h4yTtX589Qn2aQLZ9ARCPsz72')
+// This is the same for everyone
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('Hkwp8bBwt23BgLWSHq9f4BfcYy4ih1tdzH2Ggq6tVGJv')
---
+++ lib/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    id: 'pound',
+    name: 'Pound Cake',
+    description: 'Topped with a simple glaze',
+    unitName: 'box', // shows after the price, eg. 0.05 SOL/box
+    priceSol: 0.6,
+    priceUsd: 60,
+  },
+  {
+    id: 'butter',
+    name: 'Butter Cake',
+    description: 'Classic yellow cake with chocolate frosting',
+    unitName: 'box',
+    priceSol: 0.2,
+    priceUsd: 20,
+  },
+  {
+    id: 'chiffon',
+    name: 'Chiffon Cake',
+    description: 'Tender crumb and rich flavor',
+    unitName: 'box',
+    priceSol: 0.5,
+    priceUsd: 50,
+  },
+  {
+    id: 'genosis',
+    name: 'Genoise Cake',
+    description: 'Moistened with a flavored syrup',
+    unitName: 'box',
+    priceSol: 0.2,
+    priceUsd: 20,
+  }
+]
